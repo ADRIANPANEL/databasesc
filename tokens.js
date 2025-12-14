f495256e8d7ee0d16525e37ad31a57c0daa4f56e
ADRIANPANEL <dian.riski2021@gmail.com>
2025-12-14 21:59:49 +0700

    Update token list

--- tokens.js
+++ tokens.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "7901527614:AAGnpFEDw3xiOBPCDkeBOlFa9VmVP0mABCk"
+    "7901527614:AAGnpFEDw3xiOBPCDkeBOlFa9VmVP0mABCk",
+    "7738723424:AAEJK-568yJgBn39stf7uaGa-E5Kk0mnEsw"
   ]
 }
